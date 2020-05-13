f3fe996845ffd2f42808164ed32a9024455d8493
vinayaganesh <42206248+vinayaganesh@users.noreply.github.com>
2020-05-13 16:46:56 +0200

    Cypress Files

---
+++ Sprint4_Github/ShopizerContactUs.js
@@ -0,0 +1,21 @@
+describe('Test av Shopizer', function () {
+    this.beforeAll('Open shopizer', function(){
+        cy.visit('http://localhost:8080/shop/');
+        cy.viewport(1300,900)
+    });
+ 
+    it ('As a customer, I want to Contact us feature', function(){
+        cy.get('a').contains("Contact us").click({ force: true });
+        cy.get('[id="name"]').type('Team Yellow');
+        cy.get('[id="email"]').type('teamyellow@gmail.com');
+        cy.get('[id="subject"]').type('Wrong Size');
+        cy.expect("Wrong Size");
+        cy.get('[id="comment"]').type('Team Yellow received Wrong size of Bag with ID 2020-001');
+        
+        cy.get('[id="submitContact"]').click({ force: true });
+        //cy.contains('Team Yellow');
+    }); 
+
+    
+});
+ 
